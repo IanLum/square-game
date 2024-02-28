187dca8973d3b3a36ae843eddf6de1ce118d3a8f
Ian Lum <ianlum314@gmail.com>
2024-02-28 16:25:22 -0500

    simple attack structure

--- src/abstract_base_classes/enemy.gd
+++ src/abstract_base_classes/enemy.gd
@@ -4,22 +4,27 @@ class_name Enemy
 @export var SPEED = 200
 
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
-@onready var re_nav_timer: Timer = $NavigationAgent2D/ReNavTimer
+@onready var re_nav_timer: Timer = $NavigationAgent2D/Timer
+@onready var attack_radius: Area2D = $AttackRadius
 @onready var player: CharacterBody2D = get_tree().current_scene.get_node("player")
 
 func _ready():
 	re_nav_timer.timeout.connect(find_path)
-	re_nav_timer.start()
 	find_path()
 
 
 func _physics_process(_delta):
-	if nav_agent.is_navigation_finished():
-		return
-	var direction = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = direction * SPEED
-	move_and_slide()
+	if !attack_radius.get_overlapping_bodies().is_empty():
+		attack()
+	else:
+		var direction = to_local(nav_agent.get_next_path_position()).normalized()
+		velocity = direction * SPEED
+		move_and_slide()
 
 
 func find_path():
 	nav_agent.target_position = player.global_position
+
+
+func attack():
+	pass
