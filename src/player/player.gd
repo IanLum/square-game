52c9b0614c9972983687e0820fa61fa5d395ceeb
Ian Lum <ianlum314@gmail.com>
2024-02-29 09:31:21 -0500

    screen flash red when hit

--- src/player/player.gd
+++ src/player/player.gd
@@ -71,6 +71,9 @@ func _unhandled_input(event):
 func take_damage(damage: int):
 	health -= damage
 	if health <= 0: die()
+	var tween = create_tween()
+	$ui/ScreenColor.color = Color(Color.RED, 0.3)
+	tween.tween_property($ui/ScreenColor, "color", Color.TRANSPARENT, 0.3)
 
 
 func die():
